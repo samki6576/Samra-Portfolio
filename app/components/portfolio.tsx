babcca2f97912f337ecc62bd42397e7d61632fdc
Samra <hismm8690@gmail.com>
2026-05-01 06:20:58 -0700

    add

--- app/components/portfolio.tsx
+++ app/components/portfolio.tsx
@@ -47,7 +47,7 @@ export default function Portfolio() {
       category: "Apps",
       url: "https://carbonledger-ai.netlify.app/",
       year: "2025",
-      image: "jk.png",
+      image: "jk.jpg",
     },
 
     {
